3bfa42097df36c085192bfdddb0529ee7634c3cd
lukas <lhofmaie@hsr.ch>
2012-03-06 18:31:45 +0100

    AccountSkeleton implements Skeleton

--- rmiOnlyServer/src/ch/hsr/objectCaching/rmiOnlyServer/AccountSkeleton.java
+++ rmiOnlyServer/src/ch/hsr/objectCaching/rmiOnlyServer/AccountSkeleton.java
@@ -2,17 +2,17 @@ package ch.hsr.objectCaching.rmiOnlyServer;
 import java.util.HashMap;
 
 
-public class AccountSkeleton {
+public class AccountSkeleton implements RMIonlySkeleton {
 	
 	private HashMap<Integer, AccountImpl> objectMap;
 
-	public String invokeMethod(MethodCall methodCall) {
+	public ReturnValue invokeMethod(MethodCall methodCall) {
 		AccountImpl accountObject = objectMap.get(methodCall.getObjectID());
 		
 		// hier muss Logik für implementiert werden um die richtige Methode auszuwählen
 		if(methodCall.getMethodName().equalsIgnoreCase("getBalance")){
 			int b = accountObject.getBalance();
-			return String.valueOf(b);
+			
 		} 
 		return null;
 				

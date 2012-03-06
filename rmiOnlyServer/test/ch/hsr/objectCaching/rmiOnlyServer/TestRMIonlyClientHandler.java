c68ed3800835d75aea9dc911d35300466957ae33
lukas <lhofmaie@hsr.ch>
2012-03-06 18:35:46 +0100

    add RMIonlyClientHandlerTest

--- rmiOnlyServer/test/ch/hsr/objectCaching/rmiOnlyServer/TestRMIonlyClientHandler.java
+++ rmiOnlyServer/test/ch/hsr/objectCaching/rmiOnlyServer/TestRMIonlyClientHandler.java
@@ -2,12 +2,21 @@ package ch.hsr.objectCaching.rmiOnlyServer;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 import org.junit.Test;
 
 public class TestRMIonlyClientHandler {
 
 	@Test
-	public void test() {
+	public void testReadMethodCallFrom() throws IOException {
+		
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		ObjectOutputStream oos = new ObjectOutputStream(baos);
+		
+		
 		fail("Not yet implemented");
 	}
 

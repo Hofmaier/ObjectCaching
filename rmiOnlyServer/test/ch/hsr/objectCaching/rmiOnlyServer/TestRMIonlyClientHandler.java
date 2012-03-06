9b9002aa8b828c6d3a048eeb25babc0cc2cf1ceb
lukas <lhofmaie@hsr.ch>
2012-03-06 14:02:07 +0100

    add testcase to rmiOnlyServer

---
+++ rmiOnlyServer/test/ch/hsr/objectCaching/rmiOnlyServer/TestRMIonlyClientHandler.java
@@ -0,0 +1,14 @@
+package ch.hsr.objectCaching.rmiOnlyServer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestRMIonlyClientHandler {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

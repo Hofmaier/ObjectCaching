a002f4e6a4c2438d9bcb9a2a711138a849553f6d
lukas <lhofmaie@hsr.ch>
2012-03-07 19:07:52 +0100

    returnValue has Type

--- rmiOnlyServer/src/ch/hsr/objectCaching/rmiOnlyServer/ReturnValue.java
+++ rmiOnlyServer/src/ch/hsr/objectCaching/rmiOnlyServer/ReturnValue.java
@@ -1,8 +1,25 @@
 package ch.hsr.objectCaching.rmiOnlyServer;
 
-public class ReturnValue {
+import java.io.Serializable;
+
+public class ReturnValue implements Serializable{
 	
 	private Object value;
 	private int status;
 	private String message;
+	private String type;
+	
+	
+	public Object getValue() {
+		return value;
+	}
+	public void setValue(Object value) {
+		this.value = value;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
 }

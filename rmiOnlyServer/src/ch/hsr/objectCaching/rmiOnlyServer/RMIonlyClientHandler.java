cba86c7f5f76ed8b2be32f3fbf6d67bb6590fa3c
lukas <lhofmaie@hsr.ch>
2012-03-07 09:58:44 +0100

    Clienthandler uses input and outputstream instead of socket. testing works

--- rmiOnlyServer/src/ch/hsr/objectCaching/rmiOnlyServer/RMIonlyClientHandler.java
+++ rmiOnlyServer/src/ch/hsr/objectCaching/rmiOnlyServer/RMIonlyClientHandler.java
@@ -3,36 +3,38 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 public class RMIonlyClientHandler implements Runnable {
 
-	private Socket socket;
 	private RMIonlySkeleton skeleton;
 	private InputStream inputStream;
+	private OutputStream outputStream;
 	
 	public void setInputStream(InputStream inputStream) {
 		this.inputStream = inputStream;
 	}
 
-	public RMIonlyClientHandler(InputStream inputStream){
+	public RMIonlyClientHandler(InputStream inputStream, OutputStream os){
 		this.inputStream = inputStream;
+		this.outputStream = os;
 	}
 
 	@Override
 	public void run() {
 		try {
-			inputStream = socket.getInputStream();
+			
 			MethodCall methodCall = readMethodCallfrom(inputStream);
 			
 			processMethodCall(methodCall);
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
 			//toDo: send Errormessage to Client if class is unknown
 			e.printStackTrace();
 		}
-
 	}
 
 	MethodCall readMethodCallfrom(InputStream inputStream) throws IOException,
@@ -57,7 +59,7 @@ public class RMIonlyClientHandler implements Runnable {
 
 	private void sendResponse(ReturnValue returnValue) {
 		try {
-			ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+			ObjectOutputStream oos = new ObjectOutputStream(outputStream);
 			oos.writeObject(returnValue);
 		} catch (IOException e) {
 			e.printStackTrace();

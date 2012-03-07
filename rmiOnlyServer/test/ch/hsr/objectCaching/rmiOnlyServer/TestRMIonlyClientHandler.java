3d9df62fa96bdc4d35c5043bbbbf53586a175867
lukas <lhofmaie@hsr.ch>
2012-03-07 08:57:22 +0100

    test ReadMethodCallFrom

--- rmiOnlyServer/test/ch/hsr/objectCaching/rmiOnlyServer/TestRMIonlyClientHandler.java
+++ rmiOnlyServer/test/ch/hsr/objectCaching/rmiOnlyServer/TestRMIonlyClientHandler.java
@@ -2,6 +2,7 @@ package ch.hsr.objectCaching.rmiOnlyServer;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
@@ -11,13 +12,21 @@ import org.junit.Test;
 public class TestRMIonlyClientHandler {
 
 	@Test
-	public void testReadMethodCallFrom() throws IOException {
+	public void testReadMethodCallFrom() throws IOException, ClassNotFoundException {
 		
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		ObjectOutputStream oos = new ObjectOutputStream(baos);
+		MethodCall methodCall = new MethodCall();
+		methodCall.setMethodName("getBalance");
+		methodCall.setClassName("Account");
 		
+		oos.writeObject(methodCall);
 		
-		fail("Not yet implemented");
+		ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+		
+		RMIonlyClientHandler clientHandler = new RMIonlyClientHandler(bais);
+		MethodCall testCall = clientHandler.readMethodCallfrom(bais);
+		
+		assertEquals(methodCall.getMethodName(), testCall.getMethodName());
 	}
-
 }

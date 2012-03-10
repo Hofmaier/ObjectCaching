66a2b0e343d0e0662ec860341257964813fff696
unknown <rkohler@PIN1262009.hsr.ch>
2012-03-10 17:17:01 +0100

    implemented Scenarios

---
+++ testFrameworkServer/src/ch/hsr/objectCaching/testFrameworkServer/TestCase.java
@@ -0,0 +1,38 @@
+package ch.hsr.objectCaching.testFrameworkServer;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class TestCase implements Serializable
+{
+	
+	private static final long serialVersionUID = 1;
+	private String systemUnderTest;
+	private ArrayList<Scenario> scenarios;
+	
+	public TestCase(String systemUnderTest)
+	{
+		scenarios = new ArrayList<Scenario>();
+		this.systemUnderTest = systemUnderTest;
+	}
+	
+	public Scenario generateNewScenario(int id)
+	{
+		Scenario temp = new Scenario(id);
+		scenarios.add(temp);
+		return temp;
+	}
+	
+	public int getScenariosCount()
+	{
+		return scenarios.size();
+	}
+
+	public String getSystemUnderTest() {
+		return systemUnderTest;
+	}
+
+	public void setSystemUnderTest(String systemUnderTest) {
+		this.systemUnderTest = systemUnderTest;
+	}
+}

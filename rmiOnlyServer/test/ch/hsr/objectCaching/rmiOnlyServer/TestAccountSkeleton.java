badcadad9dfba1457a70a105a4b2872890d38e94
lukas <lhofmaie@hsr.ch>
2012-03-10 17:02:11 +0100

    implemented testInvokeMethodCall

--- rmiOnlyServer/test/ch/hsr/objectCaching/rmiOnlyServer/TestAccountSkeleton.java
+++ rmiOnlyServer/test/ch/hsr/objectCaching/rmiOnlyServer/TestAccountSkeleton.java
@@ -1,9 +1,14 @@
 package ch.hsr.objectCaching.rmiOnlyServer;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
+import java.lang.reflect.InvocationTargetException;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import java.lang.reflect.*;
 
 public class TestAccountSkeleton {
 
@@ -21,5 +26,32 @@ public class TestAccountSkeleton {
 		Account account = skeleton.getCalledObject(methodCall);
 		assertTrue(account == testAccount);
 	}
-
+	
+	@Test
+	public void testInvokeMethodOnObject() throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException{
+		AccountSkeleton skeleton = new AccountSkeleton();
+		Account testAccount = new Account();
+		int balance = 200;
+		testAccount.setBalance(balance);
+		
+		MethodCall methodCall = new MethodCall();
+		Class accountClass = Account.class;
+		
+		Method getBalanceMethod = null;
+		Method[] allMethods = accountClass.getMethods();
+		for(Method method:allMethods){
+			if(method.getName().equals("getBalance")){
+				getBalanceMethod = method;
+				break;
+			}
+		}
+		
+		Class[] parameterTypes = getBalanceMethod.getParameterTypes();
+		methodCall.setClassName(accountClass.getName());
+		methodCall.setMethodName(getBalanceMethod.getName());
+		methodCall.setParameterTypes(parameterTypes);
+		Object returnValue = skeleton.invokeMethodOnObject(methodCall ,testAccount);
+		Integer castedRetVal = (Integer) returnValue;
+		assertEquals(balance, returnValue);
+	}
 }

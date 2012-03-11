d8eca81ebc40bd2e0eaa0a929e1a6a8da8f4dc5c
lukas <lhofmaie@hsr.ch>
2012-03-11 09:38:15 +0100

    add type to return value

--- rmiOnlyServer/src/ch/hsr/objectCaching/rmiOnlyServer/ReturnValue.java
+++ rmiOnlyServer/src/ch/hsr/objectCaching/rmiOnlyServer/ReturnValue.java
@@ -9,7 +9,7 @@ public class ReturnValue implements Serializable{
 	 */
 	private static final long serialVersionUID = 2163526171214692070L;
 	private Object value;
-	private String type;
+	private Class<?> type;
 	
 	
 	public Object getValue() {
@@ -18,10 +18,10 @@ public class ReturnValue implements Serializable{
 	public void setValue(Object value) {
 		this.value = value;
 	}
-	public String getType() {
+	public Class<?> getType() {
 		return type;
 	}
-	public void setType(String type) {
+	public void setType(Class<?> type) {
 		this.type = type;
 	}
 }

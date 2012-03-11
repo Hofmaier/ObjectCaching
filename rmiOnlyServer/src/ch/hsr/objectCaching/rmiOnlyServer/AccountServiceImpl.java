29a591933f42a6fc872fca53d09e0bc0cf354877
lukas <lhofmaie@hsr.ch>
2012-03-11 19:35:03 +0100

    implemented AccountSericeImpl

---
+++ rmiOnlyServer/src/ch/hsr/objectCaching/rmiOnlyServer/AccountServiceImpl.java
@@ -0,0 +1,21 @@
+package ch.hsr.objectCaching.rmiOnlyServer;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import ch.hsr.objectCaching.interfaces.Account;
+import ch.hsr.objectCaching.interfaces.AccountService;
+
+public class AccountServiceImpl implements AccountService{
+	
+	private Collection<Account> accounts = new ArrayList<Account>();
+	
+	public Collection<Account> getAllAccountObjects(){
+		return accounts;
+	}
+	
+	public void addAccount(Account account){
+		accounts.add(account);
+	}
+
+}

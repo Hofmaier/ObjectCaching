76ce47d022ae2c5f08a492a392ded7c3286b95c5
lukas <lhofmaie@hsr.ch>
2012-03-11 10:56:57 +0100

    add RMIonlyServerSystem

---
+++ rmiOnlyServer/src/ch/hsr/objectCaching/rmiOnlyServer/RMIOnlyServerSystem.java
@@ -0,0 +1,14 @@
+package ch.hsr.objectCaching.rmiOnlyServer;
+
+import ch.hsr.objectCaching.interfaces.ClientHandler;
+import ch.hsr.objectCaching.interfaces.ServerSystemUnderTest;
+
+public class RMIOnlyServerSystem implements ServerSystemUnderTest {
+
+	@Override
+	public ClientHandler getClientHandlerInstance() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ rmiOnlyServer/test/ch/hsr/objectCaching/rmiOnlyServer/TestRMIonlyServerSystem.java
@@ -0,0 +1,22 @@
+package ch.hsr.objectCaching.rmiOnlyServer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestRMIonlyServerSystem {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		
+		RMIOnlyServerSystem system = new RMIOnlyServerSystem();
+		
+		fail("Not yet implemented");
+	}
+
+}

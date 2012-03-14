46b7d35b70b84d6fbf418d95b9028914f82f7546
lukas <lhofmaie@hsr.ch>
2012-03-14 08:38:13 +0100

    objectid generator starts with 3

--- rmiOnlyServer/src/ch/hsr/objectCaching/rmiOnlyServer/ObjectIDGenerator.java
+++ rmiOnlyServer/src/ch/hsr/objectCaching/rmiOnlyServer/ObjectIDGenerator.java
@@ -2,7 +2,7 @@ package ch.hsr.objectCaching.rmiOnlyServer;
 
 public class ObjectIDGenerator {
 	
-	private static int previous;
+	private static int previous = 2;
 
 	public static Integer next() {
 		return ++previous;

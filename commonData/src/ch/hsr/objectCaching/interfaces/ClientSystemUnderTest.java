4d2191c86edd8859f01eb89a098c1d81fcf3e749
lukas <lhofmaie@hsr.ch>
2012-03-15 13:29:19 +0100

    add setSocketAddress to clientUnderTest

--- commonData/src/ch/hsr/objectCaching/interfaces/ClientSystemUnderTest.java
+++ commonData/src/ch/hsr/objectCaching/interfaces/ClientSystemUnderTest.java
@@ -1,7 +1,10 @@
 package ch.hsr.objectCaching.interfaces;
 
+import java.net.InetSocketAddress;
+
 public interface ClientSystemUnderTest {
 	
 	public AccountService getAccountService();
+	public void setServerSocketAdress(InetSocketAddress socketAdress);
 
 }
--- rmiOnlyClient/src/ch/hsr/objectCaching/rmiOnlyClient/RMIonlyClientSystem.java
+++ rmiOnlyClient/src/ch/hsr/objectCaching/rmiOnlyClient/RMIonlyClientSystem.java
@@ -1,5 +1,7 @@
 package ch.hsr.objectCaching.rmiOnlyClient;
 
+import java.net.InetSocketAddress;
+
 import ch.hsr.objectCaching.interfaces.AccountService;
 import ch.hsr.objectCaching.interfaces.ClientSystemUnderTest;
 
@@ -8,5 +10,11 @@ public class RMIonlyClientSystem implements ClientSystemUnderTest {
 	@Override
 	public AccountService getAccountService() {
 		return new AccountServiceStub();
+	}
+
+	@Override
+	public void setServerSocketAdress(InetSocketAddress socketAdress) {
+		// TODO Auto-generated method stub
+		
 	}
 }

a537007c095525dd7ea0e4e0d543e251fa1f0c32
lukas <lhofmaie@hsr.ch>
2012-03-18 18:33:05 +0100

    provide objectstream to rmiclient

--- rmiOnlyClient/src/ch/hsr/objectCaching/rmiOnlyClient/StreamProvider.java
+++ rmiOnlyClient/src/ch/hsr/objectCaching/rmiOnlyClient/StreamProvider.java
@@ -9,41 +9,39 @@ import java.net.UnknownHostException;
 public class StreamProvider implements IStreamProvider {
 	
 	private Socket socket;
+	private ObjectOutputStream objectOutputStream;
+	private ObjectInputStream objectInputStream; 
 
 	@Override
 	public ObjectOutputStream getObjectOutputStream() {
-		try {
+		
 			if(socket == null){
 			initSocket();
 			}
-			ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-			return oos;
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
+			return objectOutputStream;
 	}
 
-	private void initSocket() throws UnknownHostException, IOException {
-		socket = new Socket("localhost", 12345);
+	private void initSocket() {
+		try {
+			socket = new Socket("localhost", 12345);
+		objectInputStream = new ObjectInputStream(socket.getInputStream());
+		objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public ObjectInputStream getObjectInputStream() {
-		try {
+	
 		if(socket == null){
 				initSocket();
 		}
-				ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-				return ois;
-			} catch (UnknownHostException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		return null;
+		return objectInputStream;
 	}
 
 }

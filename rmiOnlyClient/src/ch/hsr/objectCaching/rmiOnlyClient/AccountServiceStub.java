1f5dc4b1e6bb7d64833cfb1c555febd66d567626
lukas <lhofmaie@hsr.ch>
2012-03-20 12:35:16 +0100

    accountskeleton uses creates arraylist to send objects over network. hashmap can't be serialized

--- rmiOnlyClient/src/ch/hsr/objectCaching/rmiOnlyClient/AccountServiceStub.java
+++ rmiOnlyClient/src/ch/hsr/objectCaching/rmiOnlyClient/AccountServiceStub.java
@@ -3,7 +3,6 @@ package ch.hsr.objectCaching.rmiOnlyClient;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Collection;
 

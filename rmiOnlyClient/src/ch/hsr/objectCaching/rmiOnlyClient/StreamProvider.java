aa84aeb3f39a20ef1d52f20acb82de73ce77711f
lukas <lhofmaie@hsr.ch>
2012-03-20 12:32:08 +0100

    streamprovider creates new socket wenn getallaccount is called. it works this way

--- rmiOnlyClient/src/ch/hsr/objectCaching/rmiOnlyClient/StreamProvider.java
+++ rmiOnlyClient/src/ch/hsr/objectCaching/rmiOnlyClient/StreamProvider.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class StreamProvider implements IStreamProvider {
 	
@@ -14,34 +13,18 @@ public class StreamProvider implements IStreamProvider {
 
 	@Override
 	public ObjectOutputStream getObjectOutputStream() {
-		
-			if(socket == null){
-			initSocket();
-			}
-			return objectOutputStream;
-	}
-
-	private void initSocket() {
 		try {
 			socket = new Socket("localhost", 12345);
-		objectInputStream = new ObjectInputStream(socket.getInputStream());
-		objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+			objectInputStream = new ObjectInputStream(socket.getInputStream());
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return objectOutputStream;
 	}
 
 	@Override
 	public ObjectInputStream getObjectInputStream() {
-	
-		if(socket == null){
-				initSocket();
-		}
 		return objectInputStream;
 	}
-
 }

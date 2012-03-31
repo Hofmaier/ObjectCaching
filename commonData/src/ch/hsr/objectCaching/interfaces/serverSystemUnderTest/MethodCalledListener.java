cdbec5e93633f83ce4ff31a21fc733a717859d05
lukas <lhofmaie@hsr.ch>
2012-03-31 17:47:41 +0200

    add MethodCalledListener

---
+++ commonData/src/ch/hsr/objectCaching/interfaces/serverSystemUnderTest/MethodCalledListener.java
@@ -0,0 +1,7 @@
+package ch.hsr.objectCaching.interfaces.serverSystemUnderTest;
+
+public interface MethodCalledListener {
+	
+	public void methodCalled(String methodName, String clientIPAddress);
+
+}

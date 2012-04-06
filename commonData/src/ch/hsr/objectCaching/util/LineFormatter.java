b356777b4fe1add82ca5c9a78f6e019acfa4b365
Timon Brüllmann <tbruellm@hsr.ch>
2012-04-06 12:48:29 +0200

    smaller modifications at the LineFormatter (Layout problem solved)

--- commonData/src/ch/hsr/objectCaching/util/LineFormatter.java
+++ commonData/src/ch/hsr/objectCaching/util/LineFormatter.java
@@ -17,19 +17,19 @@ public class LineFormatter extends Formatter{
 
 	@Override
 	public String format(LogRecord record) {
-		StringBuffer buffer = new StringBuffer();
+		StringBuilder buffer = new StringBuilder();
 		date.setTime(record.getMillis());
 		
 		buffer.append(date.toString().substring(4,19) + " ");
-		buffer.append(fill(record.getLevel().toString(), 7) + " ");
-		buffer.append(fill(record.getSourceClassName() + ":" + record.getSourceMethodName(), 40) + " -> ");
+		buffer.append(fill(record.getLevel().toString(), 8) + " ");
+		buffer.append(fill(record.getSourceClassName() + ":" + record.getSourceMethodName(), 80) + " --> ");
 		buffer.append(record.getMessage() + lineSeperator);
 		return buffer.toString();
 	}
 
 
 	private String fill(String string, int i) {
-		StringBuffer buffer = new StringBuffer(string);
+		StringBuilder buffer = new StringBuilder(string);
 		while(buffer.length() < i){
 			buffer.append(" ");
 		}

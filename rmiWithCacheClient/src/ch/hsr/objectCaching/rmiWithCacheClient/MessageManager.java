b0810e0b9d7e9707edf9ab1a20cd3dbb904e211c
lukas <lhofmaie@hsr.ch>
2012-04-11 09:10:22 +0200

    add messageManager

---
+++ rmiWithCacheClient/src/ch/hsr/objectCaching/rmiWithCacheClient/MessageManager.java
@@ -0,0 +1,11 @@
+package ch.hsr.objectCaching.rmiWithCacheClient;
+
+import ch.hsr.objectCaching.dto.MethodCall;
+
+public class MessageManager {
+
+	public void sendMessageCall(MethodCall methodCall) {
+		
+	}
+
+}

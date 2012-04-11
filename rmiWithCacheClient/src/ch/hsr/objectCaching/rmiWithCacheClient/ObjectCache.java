e1f1e4efd3997d49774fe70b4b75697e1ed080f4
lukas <lhofmaie@hsr.ch>
2012-04-11 15:34:36 +0200

    implemented getObject in Cache

--- rmiWithCacheClient/src/ch/hsr/objectCaching/rmiWithCacheClient/ObjectCache.java
+++ rmiWithCacheClient/src/ch/hsr/objectCaching/rmiWithCacheClient/ObjectCache.java
@@ -1,7 +1,21 @@
 package ch.hsr.objectCaching.rmiWithCacheClient;
 
+import java.util.HashMap;
+
+import ch.hsr.objectCaching.account.Account;
+import ch.hsr.objectCaching.account.AccountImpl;
+
 public class ObjectCache {
 	
-	
+	private MessageManager messageManager;
+	private HashMap<Integer, AccountImpl> objectCache;
+
+	public void setMessageManager(MessageManager messageManager) {
+		this.messageManager = messageManager;
+	}
+
+	public Account getObject(int objectID) {
+		return objectCache.get(objectID);
+	}
 
 }

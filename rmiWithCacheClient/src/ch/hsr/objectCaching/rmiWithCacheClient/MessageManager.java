520af598889808ba020ef07aec3576f0d929e850
lukas <lhofmaie@hsr.ch>
2012-04-11 15:32:32 +0200

    added receiveMethodCall to MessageManager

--- rmiWithCacheClient/src/ch/hsr/objectCaching/rmiWithCacheClient/MessageManager.java
+++ rmiWithCacheClient/src/ch/hsr/objectCaching/rmiWithCacheClient/MessageManager.java
@@ -1,11 +1,16 @@
 package ch.hsr.objectCaching.rmiWithCacheClient;
 
 import ch.hsr.objectCaching.dto.MethodCall;
+import ch.hsr.objectCaching.dto.ReturnValue;
 
 public class MessageManager {
 
 	public void sendMessageCall(MethodCall methodCall) {
 		
+	}
+
+	public ReturnValue receiveMethodCallResponse() {
+		return null;
 	}
 
 }

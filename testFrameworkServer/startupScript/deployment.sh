252ae793d4be418c5bb1c02e3d38f37ef61e10d4
Raphi Kohler <rkohler@hsr.ch>
2012-04-11 15:08:10 +0200

    TestFrameWorkServer now stops running after all CLients shut down

--- testFrameworkServer/startupScript/deployment.sh
+++ testFrameworkServer/startupScript/deployment.sh
@@ -30,6 +30,11 @@ function func_create_CLient_List
   cat ${initFile} | grep "Client*[0-9]" | awk -F"=" '{ print $2 }'> ${clientTemp} 
 }
 
+function func_get_CLient_RMI_Port
+{
+  clientPort=$( cat ${initFile} | grep "Clientport" | awk -F"=" '{ print $2 }' )
+}
+
 function func_rm
 {
   for i in `cat ${clientTemp}`
@@ -62,7 +67,7 @@ function func_startClient
 {
   for i in `cat "${startFolder}/${clientTemp}"`
   do
-    ssh student@${i} "java -jar ${remotePath}/${clientJar}" &
+    ssh student@${i} "java -jar ${remotePath}/${clientJar} ${clientPort}" &
   echo "STARTUPSCRIPT: Client with ${i} started"
   done
   echo "STARTUPSCRIPT: All clients started"
@@ -71,6 +76,8 @@ ls ${serverJarPath} | grep $1 > /dev/null 2>&1
 if [[ $? -eq 0 || "$1" = "" ]]
 then
 	func_create_CLient_List
+	sleep 1
+	func_get_CLient_RMI_Port
 	sleep 1
 	func_rm
 	sleep 2

10088d38a7ee4dd5c68eba91abfccb82f77efa6f
lukas <lhofmaie@hsr.ch>
2012-04-12 16:53:19 +0200

    implemented testGetObject

--- rmiWithCacheClient/test/ch/hsr/objectCaching/rmiWithCacheClient/TestObjectCache.java
+++ rmiWithCacheClient/test/ch/hsr/objectCaching/rmiWithCacheClient/TestObjectCache.java
@@ -1,9 +1,11 @@
 package ch.hsr.objectCaching.rmiWithCacheClient;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import ch.hsr.objectCaching.dto.ObjectRequest;
 
 public class TestObjectCache {
 
@@ -12,8 +14,16 @@ public class TestObjectCache {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetObject() {
+		int objectID = 3;
+		ObjectCache objectCache = new ObjectCache();
+		MessageManagerFake messageManager = new MessageManagerFake();
+		objectCache.setMessageManager(messageManager);
+		objectCache.getObject(objectID);
+		ObjectRequest objectRequest = (ObjectRequest) messageManager.transferObject;
+		assertEquals(objectID, objectRequest.getObjectID());
+		
+		
 	}
 
 }

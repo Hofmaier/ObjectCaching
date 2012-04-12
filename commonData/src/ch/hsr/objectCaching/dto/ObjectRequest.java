5c666f658de5bc3268994a28c01a9feb2b503d45
lukas <lhofmaie@hsr.ch>
2012-04-12 16:37:57 +0200

    added ObjectRequest

---
+++ commonData/src/ch/hsr/objectCaching/dto/ObjectRequest.java
@@ -0,0 +1,13 @@
+package ch.hsr.objectCaching.dto;
+
+public class ObjectRequest implements TransferObject {
+	public int objectID;
+
+	public int getObjectID() {
+		return objectID;
+	}
+
+	public void setObjectID(int objectID) {
+		this.objectID = objectID;
+	}
+}

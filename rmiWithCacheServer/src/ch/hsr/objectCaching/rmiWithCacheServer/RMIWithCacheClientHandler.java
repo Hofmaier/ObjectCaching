be1af864320a336074b90ffd09d6e1dd2b3692f5
lukas <lhofmaie@hsr.ch>
2012-04-16 17:16:17 +0200

    RMIWithCacheClientHandler

---
+++ rmiWithCacheServer/src/ch/hsr/objectCaching/rmiWithCacheServer/RMIWithCacheClientHandler.java
@@ -0,0 +1,7 @@
+package ch.hsr.objectCaching.rmiWithCacheServer;
+
+import ch.hsr.objectCaching.rmiOnlyServer.RMIOnlyClientHandler;
+
+public class RMIWithCacheClientHandler extends RMIOnlyClientHandler{
+	
+}

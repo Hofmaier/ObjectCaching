c73749a6b6ce61567d69ff9671ebe1425fdd9d81
lukas <lhofmaie@hsr.ch>
2012-04-17 14:27:00 +0200

    create ObjectManager

---
+++ rmiWithCacheServer/src/ch/hsr/objectCaching/rmiWithCacheServer/ObjectManager.java
@@ -0,0 +1,13 @@
+package ch.hsr.objectCaching.rmiWithCacheServer;
+
+import ch.hsr.objectCaching.dto.ObjectRequest;
+import ch.hsr.objectCaching.dto.ObjectRequestResponse;
+
+public class ObjectManager {
+
+	public ObjectRequestResponse processObjectRequest(
+			ObjectRequest objectRequest) {
+		return null;
+	}
+
+}

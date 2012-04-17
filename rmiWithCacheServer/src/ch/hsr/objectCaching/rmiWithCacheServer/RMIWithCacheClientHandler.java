f912ecbf8ea577a3899235c3c8254d94395023af
lukas <lhofmaie@hsr.ch>
2012-04-17 14:31:12 +0200

    implemented run in cacheclienthandler

--- rmiWithCacheServer/src/ch/hsr/objectCaching/rmiWithCacheServer/RMIWithCacheClientHandler.java
+++ rmiWithCacheServer/src/ch/hsr/objectCaching/rmiWithCacheServer/RMIWithCacheClientHandler.java
@@ -1,7 +1,39 @@
 package ch.hsr.objectCaching.rmiWithCacheServer;
 
+import java.io.IOException;
+
+import ch.hsr.objectCaching.dto.ObjectRequest;
+import ch.hsr.objectCaching.dto.ObjectRequestResponse;
+import ch.hsr.objectCaching.dto.TransferObject;
 import ch.hsr.objectCaching.rmiOnlyServer.RMIOnlyClientHandler;
 
 public class RMIWithCacheClientHandler extends RMIOnlyClientHandler{
 	
+	private ObjectManager objectManager;
+
+	@Override
+	public void run() {
+			try {
+				Object objectFromStream = null;
+				while ((objectFromStream = objectInputStream.readObject()) != null) {
+					TransferObject transferObject = (TransferObject) objectFromStream;
+					
+					processTransferObject(transferObject);
+					
+				}
+				objectInputStream.close();
+				objectOutputStream.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+
+	private void processTransferObject(Object objectFromStream) {
+		if(objectFromStream instanceof ObjectRequest){
+			ObjectRequest objectRequest = (ObjectRequest) objectFromStream;
+			ObjectRequestResponse response = objectManager.processObjectRequest(objectRequest);
+		}
+	}
 }

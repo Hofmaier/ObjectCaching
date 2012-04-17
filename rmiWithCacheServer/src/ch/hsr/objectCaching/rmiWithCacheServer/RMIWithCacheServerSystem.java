d41e8469cb446320fa4a0435f73c138463d3cd07
lukas <lhofmaie@hsr.ch>
2012-04-17 12:51:51 +0200

    implemented getClientHandlerInstance

--- rmiWithCacheServer/src/ch/hsr/objectCaching/rmiWithCacheServer/RMIWithCacheServerSystem.java
+++ rmiWithCacheServer/src/ch/hsr/objectCaching/rmiWithCacheServer/RMIWithCacheServerSystem.java
@@ -1,8 +1,15 @@
 package ch.hsr.objectCaching.rmiWithCacheServer;
 
+import ch.hsr.objectCaching.interfaces.ClientHandler;
+import ch.hsr.objectCaching.rmiOnlyServer.RMIOnlyClientHandler;
 import ch.hsr.objectCaching.rmiOnlyServer.RMIOnlyServerSystem;
 
 public class RMIWithCacheServerSystem extends RMIOnlyServerSystem {
 
-
+	@Override
+	public ClientHandler getClientHandlerInstance() {
+		RMIWithCacheClientHandler clientHandler = new RMIWithCacheClientHandler();
+		return clientHandler;
+	}
+	
 }

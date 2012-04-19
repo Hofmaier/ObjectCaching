bf0050e63cdc71a7425c779c82a25ad33da38587
lukas <lhofmaie@hsr.ch>
2012-04-19 14:18:36 +0200

    ObjectRequest has clienthandler

--- commonData/src/ch/hsr/objectCaching/dto/ObjectRequest.java
+++ commonData/src/ch/hsr/objectCaching/dto/ObjectRequest.java
@@ -2,8 +2,19 @@ package ch.hsr.objectCaching.dto;
 
 import java.io.Serializable;
 
+import ch.hsr.objectCaching.interfaces.ClientHandler;
+
 public class ObjectRequest implements TransferObject, Serializable {
 	private int objectID;
+	private ClientHandler clientHandler;
+	
+	public ClientHandler getClientHandler() {
+		return clientHandler;
+	}
+
+	public void setClientHandler(ClientHandler clientHandler) {
+		this.clientHandler = clientHandler;
+	}
 
 	public int getObjectID() {
 		return objectID;

39081b1d02da106c091cd71e1d346f2649876515
lukas <lhofmaie@hsr.ch>
2012-04-19 14:21:26 +0200

    moved send(object) to clienthandler

--- commonData/src/ch/hsr/objectCaching/interfaces/ClientHandler.java
+++ commonData/src/ch/hsr/objectCaching/interfaces/ClientHandler.java
@@ -1,19 +1,20 @@
 package ch.hsr.objectCaching.interfaces;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.OutputStream;
+
+import ch.hsr.objectCaching.dto.Update;
 
 public class ClientHandler implements Runnable {
 	
 	protected InputStream inputStream;
 	protected OutputStream outputStream;
-	public void setInputStream(InputStream inputStream) {
-		this.inputStream = inputStream;
-	}
-
-	public void setOutputStream(OutputStream outputStream) {
-		this.outputStream = outputStream;
-	}
+	protected ObjectOutputStream objectOutputStream;
+	protected ObjectInputStream objectInputStream;
+	
 
 	@Override
 	public void run() {
@@ -21,5 +22,31 @@ public class ClientHandler implements Runnable {
 	}
 	
 	public void setClientIpAddress(String clientIpAddress){}
+
+	public void send(Object object) {
+		try {
+			objectOutputStream.writeObject(object);
+			objectOutputStream.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+	}
+
+	public void setInputStream(InputStream inputStream) {
+		try {
+			objectInputStream = new ObjectInputStream(inputStream);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void setOutputStream(OutputStream outputStream) {
+		this.outputStream = outputStream;
+		try {
+			objectOutputStream = new ObjectOutputStream(outputStream);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
 }

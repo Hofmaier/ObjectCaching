4f633966abf6fe3a7642bc8db3e5759116c4c3b6
lukas <lhofmaie@hsr.ch>
2012-04-24 10:32:34 +0200

    get clientip address in clienthandler interface

--- commonData/src/ch/hsr/objectCaching/interfaces/ClientHandler.java
+++ commonData/src/ch/hsr/objectCaching/interfaces/ClientHandler.java
@@ -6,8 +6,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 
-import ch.hsr.objectCaching.dto.ObjectUpdate;
-
 public class ClientHandler implements Runnable {
 	
 	protected InputStream inputStream;
@@ -22,6 +20,7 @@ public class ClientHandler implements Runnable {
 	}
 	
 	public void setClientIpAddress(String clientIpAddress){}
+	public String getClientIpAddress() {return null;}
 
 	public void send(Object object) {
 		try {
